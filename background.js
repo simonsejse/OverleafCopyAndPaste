2a3e670baa9946fa15e59aaa66b46c7c5c29296f
Temp-Simon <simonsejse@gmail.com>
2023-07-24 01:37:36 +0200

    Now you can upload one image, needs functionaly for multiple (also hardcoded folder path)

--- background.js
+++ background.js
@@ -3,53 +3,64 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-console.log('Background script has been loaded');
+console.log("Background script has been loaded");
 
-
-function getCookie(name, callback) {
-    chrome.cookies.get({
-      url: "https://www.overleaf.com",
-      name: name
-    }, function(cookie) {
-      if (cookie) {
-        callback(null, cookie.value);
-      } else {
-        callback('Cannot find cookie.');
-      }
-    });
+function dataURItoBlob(dataURI) {
+  var byteString = atob(dataURI.split(",")[1]);
+  var mimeString = dataURI.split(",")[0].split(":")[1].split(";")[0];
+  var ab = new ArrayBuffer(byteString.length);
+  var ia = new Uint8Array(ab);
+  for (var i = 0; i < byteString.length; i++) {
+    ia[i] = byteString.charCodeAt(i);
   }
-  
+  return new Blob([ab], { type: mimeString });
+}
 
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    if (request.message === "CTRL_V_PRESSED") {
-        const csrfToken = request.csrfToken;
-        const url = `https://www.overleaf.com/${request.url.substring(1)} +"&_csrf=" + ${csrfToken}`;
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  if (request.message === "CTRL_V_PRESSED") {
+    const csrfToken = request.csrfToken;
+    const url = `https://www.overleaf.com/${request.url.substring(
+      1
+    )}?folder_id=64bd9968f3fa586811992be8`;
 
-        getCookie("overleaf_session2", function(err, cookieValue) {
-            if (err) {
-            console.log(err);
-            return;
-            }
-            fetch(url, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Set-Cookie': `overleaf_session2=${cookieValue}`  
-                },
-                body: JSON.stringify({url: request.url})
-            }).then(response => {
-                console.log("Response: ", response);
-                sendResponse({message: "URL sent to server"});
-            });
-        });
-    }
+    let formData = new FormData();
+    formData.append("targetFolderId", request.images[0].targetFolderId);
+    formData.append("name", request.images[0].name);
+    formData.append("type", request.images[0].type);
+
+    const blob = dataURItoBlob(request.images[0].qqfile);
+    formData.append("qqfile", blob, request.images[0].name);
+
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "X-CSRF-Token": csrfToken, // include in URL or headers > for simplicity, headers is more readable
+      },
+      credentials: "include", // include cookies in the request
+      body: formData,
+    })
+      .then((response) => {
+        console.log("Am i reached?");
+        return response.json();
+      })
+      .then((data) => {
+        console.log("Am i reached 2?");
+        console.log("Response: ", data);
+        sendResponse({ message: "URL sent to server" });
+      })
+      .catch((error) => {
+        console.log("Error: ", error);
+      });
+  }
+  // to enable asynchronous use of 'sendResponse', return true
+  return true;
 });
--- content-scripts.js
+++ content-scripts.js
@@ -3,9 +3,9 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -13,19 +13,75 @@
 // limitations under the License.
 
 function isPasteEvent(e) {
-  return (e.ctrlKey || e.metaKey) && e.key === 'v';
+  return (e.ctrlKey || e.metaKey) && e.key === "v";
 }
 
-document.addEventListener('keydown', function(e) {
-  if (!(document.URL.startsWith("https://www.overleaf.com") 
-      || document.URL.startsWith("https://overleaf.com") 
-      || document.URL.startsWith('http://www.overleaf.com')
-      || document.URL.startsWith('http://overleaf.com'))){
-        return; // Do nothing if not on overleaf.com
+async function getClipboardItems() {
+  const clipboardItems = await navigator.clipboard.read();
+  return clipboardItems;
+}
+
+async function getImages(clipboardItems) {
+  let images = [];
+  for (let item of clipboardItems) {
+    for (let type of item.types) {
+      if (type.startsWith("image/")) {
+        let blob = await item.getType(type);
+        const base64Data = await blobToBase64(blob);
+
+        let image = {
+          targetFolderId: "64bd9968f3fa586811992be8",
+          name: "somename.png",
+          type: blob.type,
+          qqfile: base64Data,
+        };
+
+        images.push(image);
+      }
+    }
   }
-  if (isPasteEvent(e)) {
-    var url = document.location.pathname + "/upload?folder_id=assets";
-    var csrfToken = document.querySelector('meta[name="ol-csrfToken"]').content;
-    chrome.runtime.sendMessage({message: "CTRL_V_PRESSED", url: url, csrfToken: csrfToken});
+  return images;
+}
+
+function blobToBase64(blob) {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onloadend = () => {
+      resolve(reader.result);
+    };
+    reader.onerror = reject;
+    reader.readAsDataURL(blob); //Base64
+  });
+}
+
+document.addEventListener("keydown", function (e) {
+  if (!isPasteEvent(e)) return;
+
+  if (
+    !(
+      document.URL.startsWith("https://www.overleaf.com") ||
+      document.URL.startsWith("https://overleaf.com") ||
+      document.URL.startsWith("http://www.overleaf.com") ||
+      document.URL.startsWith("http://overleaf.com")
+    )
+  ) {
+    return; // Do nothing if not on overleaf.com
   }
+  (async function () {
+    const clipboardItems = await getClipboardItems();
+    const images = await getImages(clipboardItems);
+
+    if (images) {
+      var url = document.location.pathname + "/upload";
+      var csrfToken = document.querySelector(
+        'meta[name="ol-csrfToken"]'
+      ).content;
+      chrome.runtime.sendMessage({
+        message: "CTRL_V_PRESSED",
+        url: url,
+        csrfToken: csrfToken,
+        images: images,
+      });
+    }
+  })();
 });
