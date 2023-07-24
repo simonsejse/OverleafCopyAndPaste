5223b7f277571d823facfec0fff22d4ba8f6c0fc
Cody Dunne <cody.g.dunne@gmail.com>
2023-07-24 12:20:42 -0400

    Add "figs" and "img" folders

--- content-scripts.js
+++ content-scripts.js
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-const foldersByPriority = ["assets", "images", "pictures", "figures"];
+const foldersByPriority = ["assets", "images", "pictures", "figures", "figs", "img"];
 
 function getImageFolderId() {
   return new Promise((resolve, reject) => {
